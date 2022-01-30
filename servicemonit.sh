b1de129b25dcfae3bbf53ba6bd0e803069b6e8a5
yuvalakshman66 <98636131+yuvalakshman66@users.noreply.github.com>
2022-01-30 13:31:54 +0530

    Create servicemonit.sh

---
+++ servicemonit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+svc=apache2
+svcstat=not
+service $svc status | grep -i "running\|'not running'" | awk '{print $3}' | while read output;
+do
+echo $output
+if [ "$output" == "$svcstat" ]; then
+    service $svc start
+    echo "$svc service is UP now.!" | mail -s "$svc service was DOWN and restarted now On $(hostname)" khadarmd7@gmail.com #should install mailutils and setup before proceeding
+    else
+    echo "$serv service is running"
+    fi
+done
